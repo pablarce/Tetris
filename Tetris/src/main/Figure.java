e9a717393bceb26195926b2a4510769596a6d3b0
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2023-05-14 23:13:56 +0200

    some adjustments

--- Tetris/src/main/Figure.java
+++ Tetris/src/main/Figure.java
@@ -32,4 +32,5 @@ public abstract class Figure {
         this.definition = newDefinition;
     }
 
+
 }
--- Tetris/src/main/Prueba.java
+++ Tetris/src/main/Prueba.java
@@ -3,8 +3,12 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import main.Shapes.*;
 
 public class Prueba{
+    private ArrayList<Figure> figures = new ArrayList<>();
     private JFrame frame;
     private JPanel figurePanel;
     private JPanel gamePanel;
@@ -27,7 +31,7 @@ public class Prueba{
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.setColor(Color.WHITE);
+                g.setColor(Color.white);
                 g.fillRect(0, 0, gamePanelWidth, gamePanelHeight);
                 g.setColor(Color.BLACK);
                 g.drawRect(0, 0, gamePanelWidth, gamePanelHeight);
@@ -40,7 +44,8 @@ public class Prueba{
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.setColor(Color.YELLOW);
+                Color selectedColor = new Color(0x00FF00);
+                g.setColor(selectedColor);
                 g.fillRect(pixelX, pixelY, pixelWidth, pixelHeight);
                 g.setColor(Color.BLACK);
                 g.drawRect(pixelX, pixelY, pixelWidth, pixelHeight);
@@ -64,12 +69,12 @@ public class Prueba{
                         }
                         break;
                     case KeyEvent.VK_LEFT:
-                        if (pixelX > 10) {
+                        if (pixelX > 10 && pixelY + pixelHeight < bottomLimit) {
                             pixelX -= 20;
                         }
                         break;
                     case KeyEvent.VK_RIGHT:
-                        if (pixelX + pixelWidth < gamePanelWidth - 10) {
+                        if (pixelX + pixelWidth < gamePanelWidth - 10 && pixelY + pixelHeight < bottomLimit) {
                             pixelX += 20;
                         }
                         break;
@@ -96,5 +101,28 @@ public class Prueba{
         gamePanel.requestFocusInWindow();
 
         frame.setVisible(true);
+    }
+
+    public static Figure generateFigure(){
+        Random random = new Random();
+        int figure = random.nextInt(7);
+        switch (figure) {
+            case 0:
+                return new Square("red", 0, 0);
+            case 1:
+                return new Tshape("blue", 10,10);
+            case 2:
+                return new Straigth("green", 20,20);
+            case 3:
+                return new Lshape("yellow", 30,30);
+            case 4:
+                return new Sshape("orange", 40,40);
+            case 5:
+                return new Zshape("purple", 50,50);
+            case 6:
+                return new Jshape("pink", 60,60);
+            default:
+                return null;
+        }
     }
 }
